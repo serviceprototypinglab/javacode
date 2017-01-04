caa7fd7b0ff487f909bbafcb29bbea5b59fbcd62
spio <spio@tiguriner>
2017-01-04 17:04:31 +0100

    - another tiny project added

---
+++ maths/Maths.java
@@ -0,0 +1,10 @@
+import wizardmath.MathWizard;
+
+public class Maths
+{
+	public final static void main(String[] args)
+	{
+		MathWizard mw = new MathWizard();
+		System.out.println(mw.sum(1, 1));
+	}
+}
---
+++ maths/src/MathWizard.java
@@ -0,0 +1,13 @@
+package wizardmath;
+
+public class MathWizard
+{
+	public MathWizard()
+	{
+	}
+
+	public int sum(int s1, int s2)
+	{
+		return s1 + s2;
+	}
+}
