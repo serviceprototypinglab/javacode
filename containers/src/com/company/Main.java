5d76b683adc7eebb89fead1357b61be3f0f6154d
spio <spio@tiguriner>
2017-01-05 10:47:22 +0100

    - addition of the containers sample project - complementation by mostly handcrafted Maven file

---
+++ containers/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Box box0 = new Box();
+        Box box1 = new Box(1, 1, 1);
+        Box box2 = new Box(2, 3, 4);
+        BoxContainer boxContainer = new BoxContainer();
+        boxContainer.addBox(box0);
+        boxContainer.addBox(box1);
+        boxContainer.addBox(box2);
+        System.out.println("The total boxes volume is " + boxContainer.totalVolume());
+    }
+}
